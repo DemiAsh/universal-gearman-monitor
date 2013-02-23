c67253c4c2570574058eaf0baa5a5145938cf7bd
NosFire <>
2013-02-24 01:33:18 +0300

    add error page and remove some warning

--- index.php
+++ index.php
@@ -1,27 +1,20 @@
 <?php
 
-error_reporting(65535);
-ini_set("display_errors", 1);
 
 require_once __DIR__ . '/lib/class_GearmanStatus.php';
 require_once __DIR__ . '/lib/Twig/Autoloader.php';
 Twig_Autoloader::register();
 
 $loader = new Twig_Loader_Filesystem(__DIR__ . '/themes/views');
-$twig = new Twig_Environment($loader, array(
-		'debug' => true,
-	));
-$twig->addExtension(new Twig_Extension_Debug());
-
-//$twig->addGlobal('base', 'http://'.$_SERVER['HTTP_HOST'].'/themes/media');
+$twig = new Twig_Environment($loader);
 
 $gearmand = new GearmanStatus();
 
 if( $gearmand->getStatus() )
 {
 	echo $twig->render('index.twig', array(
-		'status' => $gearmand->getStatus()->status,
-		'workers' => $gearmand->getStatus()->workers,
+		'status' => (isset($gearmand->getStatus()->status) ? $gearmand->getStatus()->status : null),
+		'workers' => (isset($gearmand->getStatus()->workers) ? $gearmand->getStatus()->workers : null),
 	));
 }
 else
